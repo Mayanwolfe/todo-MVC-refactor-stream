b2c1242bdddfa8c7c4e71b674ff95f46750467bc
Claire Bourdon <cebourdon625@gmail.com>
2022-08-17 23:01:41 -0400

    finished

--- server.js
+++ server.js
@@ -23,44 +23,8 @@ app.use(express.urlencoded({ extended: true }));
 
 //Set Routes
 app.use('/', homeRoutes)
-//app.use('/edit', editRoutes)
+app.use('/edit', editRoutes)
 
-
-
-// //UPDATE METHOD
-// app
-//     .route("/edit/:id")
-//     .get((req, res) => {
-//         const id = req.params.id;
-//         TodoTask.find({}, (err, tasks) => {
-//             res.render("edit.ejs", { todoTasks: tasks, idTask: id });
-//         });
-//     })
-//     .post((req, res) => {
-//         const id = req.params.id;
-//         TodoTask.findByIdAndUpdate(
-//             id,
-//             {
-//                 title: req.body.title,
-//                 content: req.body.content
-//             },
-
-//             err => {
-//                 if (err) return res.status(500).send(err);
-//                 res.redirect("/");
-//             });
-//     });
-
-// //DELETE
-// app
-//     .route("/remove/:id")
-//     .get((req, res) => {
-//         const id = req.params.id;
-//         TodoTask.findByIdAndRemove(id, err => {
-//             if (err) return res.send(500, err);
-//             res.redirect("/");
-//         });
-//     });
 
 //Start Server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
